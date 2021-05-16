69915e4bc6d0947ef6cd7f8a115454c33d26ac22
Zivilevs <zivileval@yahoo.com>
2021-05-16 17:53:27 -0300

    update style and % for negative

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -2,10 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 ld = (number % 10)
+if number < 0:
+    ld = 10 - ld
 if ld > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, ld))
 elif ld == 0:
     print("Last digit of {} is 0 and is 0".format(number, ld))
 else:
-    print(
-    "Last digit of {} is {} and is less than 6 and not 0".format(number, ld))
+    print("Last digit of {} is {} and is less than 6 and not 0"
+          .format(number, ld))
