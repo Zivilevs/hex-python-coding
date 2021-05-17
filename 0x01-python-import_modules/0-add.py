0dba708bd7e5159b5e6d7b53bc13b051047e4f44
Zivilevs <zivileval@yahoo.com>
2021-05-16 23:19:09 -0300

    correction for test

--- 0x01-python-import_modules/0-add.py
+++ 0x01-python-import_modules/0-add.py
@@ -9,7 +9,7 @@ a = 1       # a, b must be before name == main,
 b = 2       # if they are used in program later.
 
 if __name__ == 'add_0':
-    add(a, b)
+    __name__.add(a, b)
 # '__main__' is name of this file, but in in this case
 # it is wanted to be specific.
 print("{} + {} = {}".format(a, b, add(a, b)))
