622f4d782c243cdfe016beb0888e313f6d479bec
Zivilevs <zivileval@yahoo.com>
2021-05-17 23:30:36 -0300

    remove excess

--- 0x02-python-data_structures/1-print_matrix_integer.py
+++ 0x02-python-data_structures/1-print_matrix_integer.py
@@ -5,13 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         j = 0
         while j < (len(matrix[i]) - 2):
-            print("{:d} {:d} {:d}$".format(
+            return print("{:d} {:d} {:d}".format(
                   matrix[i][j], matrix[i][j + 1], matrix[i][j + 2]))
             j += 1
-            i = 0
-    while i < (len(matrix[0]) - 1):
-        print('-', end="")
-        i += 1
-    else:
-        print('$')
-    return ('$')
+
