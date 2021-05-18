bdd9f9b930e024c6c414223f9aeeda2fe721f16d
Zivilevs <zivileval@yahoo.com>
2021-05-17 22:27:39 -0300

    remove some letters from word

---
+++ 0x02-python-data_structures/0-no_c.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    c_list = ['c', 'C']
+    my_list = list(my_string)
+    noC_list = []
+    for i in my_list:
+        if i not in c_list:
+            noC_list.append(i)
+    i = 0
+    noC_string = ""
+
+    while (i < len(noC_list)):
+        noC_string += str(noC_list[i])
+        i += 1
+    return noC_string
---
+++ 0x02-python-data_structures/README.md
@@ -0,0 +1,5 @@
+# Data Structures: Lists, Tuples
+
+1. Write a function that removes all characters c and C from a string. 
+
+   It is not allowed to import any module or  to use `str.replace()`)
