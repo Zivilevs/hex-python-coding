c714388de726a5d5cc22ca56453977362e2f4334
Zivilevs <zivileval@yahoo.com>
2021-05-17 23:26:19 -0300

    print noce matrix

--- 0x02-python-data_structures/0-no_c.py
+++ 0x02-python-data_structures/0-no_c.py
@@ -5,7 +5,7 @@ def no_c(my_string):
     c_list = ['c', 'C']
     my_list = list(my_string)
     noC_list = []
-    for i in my_list:
+    for i in (my_list):
         if i not in c_list:
             noC_list.append(i)
     i = 0
@@ -15,3 +15,5 @@ def no_c(my_string):
         noC_string += str(noC_list[i])
         i += 1
     return noC_string
+
+print(no_c("gcc"))
---
+++ 0x02-python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        j = 0
+        while j < (len(matrix[i]) - 2):
+            print("{} {} {}$".format(
+                  matrix[i][j], matrix[i][j + 1], matrix[i][j + 2]))
+            j += 1
+            i = 0
+    while i < (len(matrix[0]) - 1):
+        print('-', end="")
+        i += 1
+    else:
+        print('$')
+    return ('$')
