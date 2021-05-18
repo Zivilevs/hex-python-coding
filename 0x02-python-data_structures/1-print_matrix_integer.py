0ba78c920da90f47950cdfac74c111514a29c70a
Zivilevs <zivileval@yahoo.com>
2021-05-17 23:27:43 -0300

    str.format update

--- 0x02-python-data_structures/1-print_matrix_integer.py
+++ 0x02-python-data_structures/1-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         j = 0
         while j < (len(matrix[i]) - 2):
-            print("{} {} {}$".format(
+            print("{:d} {:d} {:d}$".format(
                   matrix[i][j], matrix[i][j + 1], matrix[i][j + 2]))
             j += 1
             i = 0
